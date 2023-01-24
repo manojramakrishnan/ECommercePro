c7f1e63179575c76dce12863454df0109ed36b3c
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2023-01-24 11:03:09 +0530

    new module EP-MAIN added

---
+++ EP-MAIN/src/main/java/com/shopeasy/epmain/EpMainApplication.java
@@ -0,0 +1,13 @@
+package com.shopeasy.epmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpMainApplication.class, args);
+	}
+
+}
---
+++ EP-MAIN/src/test/java/com/shopeasy/epmain/EpMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopeasy.epmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
