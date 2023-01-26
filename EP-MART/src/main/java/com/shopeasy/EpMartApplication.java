e7cd13f865d22a4706e704f33f87400d7ae9644e
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2023-01-26 10:26:28 +0530

    added EP Mart Module

---
+++ EP-MART/src/main/java/com/shopeasy/EpMartApplication.java
@@ -0,0 +1,13 @@
+package com.shopeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpMartApplication.class, args);
+	}
+
+}
