58a8225cb031d524ddbc787831822e4a62ed1f05
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2023-01-26 10:11:47 +0530

    added EP Main Modules Module

---
+++ EP-MAIN-MODULES/src/main/java/com/shopeasy/EpMainModulesApplication.java
@@ -0,0 +1,13 @@
+package com.shopeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpMainModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpMainModulesApplication.class, args);
+	}
+
+}
